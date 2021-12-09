5b979801347eac4bb2f3b8b802cfc2a795924275
Jihoi-Kang <jihoi.kang@gmail.com>
2021-12-09 16:39:47 +0900

    Update cat list paging

--- app/src/main/java/com/jay/cats/ui/CatsActivity.kt
+++ app/src/main/java/com/jay/cats/ui/CatsActivity.kt
@@ -1,19 +1,20 @@
 package com.jay.cats.ui
 
 import android.os.Bundle
+import androidx.lifecycle.lifecycleScope
 import com.base.library.ui.BaseActivity
 import com.jay.cats.R
 import com.jay.cats.databinding.ActivityCatsBinding
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class CatsActivity : BaseActivity<ActivityCatsBinding, CatsViewModel>(
     R.layout.activity_cats
 ) {
-
-    private val adapter: CatsAdapter by lazy {
-        CatsAdapter()
-    }
+    private val adapter: CatsAdapter by lazy { CatsAdapter() }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,8 +28,10 @@ class CatsActivity : BaseActivity<ActivityCatsBinding, CatsViewModel>(
     }
 
     private fun setupObserve() {
-        viewModel.cats.observe(this) {
-            adapter.updateItems(it)
+        lifecycleScope.launch {
+            viewModel.fetchCats().distinctUntilChanged().collectLatest {
+                adapter.submitData(it)
+            }
         }
     }
 
